8e8b7beb9737d2a7b82463947e472e05dcca1cc9
giskook <zhangkai.gis@163.com>
2023-11-25 10:17:14 +0800

    add ecs.g7.32xlarge-1

---
+++ ali/ecs.g7.32xlarge-1/run.sh
@@ -0,0 +1,10 @@
+for i in $(seq 1 10)
+do
+       date
+       nohup ./build/zkProver -c testvectors/config_runFile_BatchProof.json > result-$i.txt 2>&1 &
+       sleep 800
+       pid=$(ps -e | grep zkProver | awk '{printf $1}')
+       kill -9 $pid
+       sleep 60
+       echo "--------------------end--------------------" >> ./zkprover.log
+done
